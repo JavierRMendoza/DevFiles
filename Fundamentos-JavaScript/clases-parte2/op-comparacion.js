3ce3b5ea7de6bec3009d1de88e9f0ecba1b0a464
JavierRMendoza <javirodriguez046@gmail.com>
2024-10-08 08:42:53 -0500

    Agregando archivo a la carpeta

---
+++ Fundamentos-JavaScript/clases-parte2/op-comparacion.js
@@ -0,0 +1,24 @@
+/*Operadores de comparacion
+
+== si es igual a
+=== verifica tanto valor y tipo de datos
+!= si es diferente de
+!== si es diferente tanto en valor y tipo de datos
+>
+<
+>=
+<=
+
+*/
+
+//Ejemplos
+const a =10;
+const b = 20;
+const c = "10"
+
+a == b;
+a === c;
+a != b;
+a !== c;
+a > b
+a <= b;
