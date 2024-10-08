bca08dcfe2826f78276ae079ea2cc175a7d3183f
JavierRMendoza <javirodriguez046@gmail.com>
2024-10-08 08:36:46 -0500

    Agregando archivos

---
+++ Fundamentos-JavaScript/clases-parte1/clase-number.js
@@ -0,0 +1,36 @@
+// Entero y decimal
+const entero = 42
+const decimal = 3.14
+console.log(typeof entero, typeof decimal)
+
+// Notación científica
+const cientifico = 5e3
+
+// infinitos y NaN
+const infinito = Infinity
+const noEsUnNumero = NaN
+
+// operaciones aritmeticas
+// suma,resta, multiplicacion y division
+const suma = 3 + 4
+const resta = 4 - 4
+const multiplicacion = 4 * 7
+const division = 16 / 2
+
+// modulo y exponenciacion
+const modulo = 15 % 8
+const exponenciacion = 2 ** 3
+
+// precision
+const resultado = 0.1 + 0.2
+console.log(resultado)
+console.log(resultado.toFixed(1))
+console.log(resultado === 0.3)
+
+// operaciones avanzadas
+const raizCuadrada = Math.sqrt(16)
+const valorAbsoluto = Math.abs(-7)
+const aleatorio = Math.random()
+console.log(raizCuadrada)
+console.log(valorAbsoluto)
+console.log(aleatorio)
