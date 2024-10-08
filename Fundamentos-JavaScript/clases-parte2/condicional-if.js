666be6b05593b52041c38293c260197e13e59438
JavierRMendoza <javirodriguez046@gmail.com>
2024-10-08 08:49:06 -0500

    Agregando archivos

---
+++ Fundamentos-JavaScript/clases-parte2/condicional-if.js
@@ -0,0 +1,18 @@
+/*
+if (let ==== algo) {
+    código
+} else if {
+    código
+} else {
+
+}*/
+
+let nombre = "Nico";
+
+if(nombre === "Diego") {
+    console.log("Hola Diego");
+} else if(nombre === "Nico") {
+    console.log("Hola Nico");
+} else {
+    console.log("Nombre no encontrado");
+}
---
+++ Fundamentos-JavaScript/clases-parte2/op-logicos.js
@@ -0,0 +1,16 @@
+/*Operadores logicos
+
+&& si se cumplen ambos
+|| se cumple alguno
+! negacion
+
+*/
+
+//Ejemplos
+const a = 10;
+const b = 20;
+const c = "10";
+
+a == b && a === c
+a != b || a === c
+!(a === c)
